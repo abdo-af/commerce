7409e69ac7c18da0ca40a6fc5c361f3a1c4c7f38
Abdelrahman Elsedemy <abdoelsedemy8@gmail.com>
2025-09-11 15:47:56 +0300

    update

---
+++ lib/core/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color backgroundcolor = Color(0xFF0F1A24);
+  static const Color primarycolor = Color(0xFF21364A);
+  static const Color secondrycolor = Color(0xFF3D99F5);
+  static const Color thirdcolor = Color(0xFF8FADCC);
+  static const Color fourthcolor = Color(0xFF21364A);
+// كدا الدارك
+  static const Color bgcolor = Color(0xFFEFF2F4);
+  static const Color color = Color(0xFF8FADCC);
+}
